dcdf06b557e147ece80c4ac4618bcb32486a9d66
agus kainer <puchidev98@gmail.com>
2026-01-14 22:44:46 -0300

    app funcional, falta el aviso por correo para no pasarse los km

--- Back/src/app.js
+++ Back/src/app.js
@@ -8,4 +8,13 @@ app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
 
+const verificarToken = require("./middleware/verificarToken");
+const authRoutes = require("./routes/authRoutes");
+const autosRoutes = require("./routes/autosRoutes");
+
+app.use(verificarToken);
+
+app.use("/api/auth", authRoutes);
+app.use("/api/autos", autosRoutes);
+
 module.exports = app;
--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -1,34 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './store'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Dashboard from './pages/Dashboard'
+import RegistroAuto from './pages/RegistroAuto'
+import HistorialCliente from './pages/HistorialCliente'
+import ProtectedRoute from './components/ProtectedRoute'
 
+function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/historial" element={<HistorialCliente />} />
+          
+          <Route element={<ProtectedRoute />}>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/registro-auto" element={<RegistroAuto />} />
+          </Route>
+        </Routes>
+      </Router>
+    </Provider>
   )
 }
 
